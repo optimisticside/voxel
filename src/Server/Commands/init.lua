1a6084c4418aacf240bc94c31373df5f067c4bec
OptimisticSide <47266822+optimisticside@users.noreply.github.com>
2021-05-09 12:07:57 -0700

    Added findCommand function

--- src/Server/Commands/init.lua
+++ src/Server/Commands/init.lua
@@ -2,11 +2,42 @@
 -- 5/9/2021
 -- Command manager
 
+local REVOKE_PREFIX = "un"
+
 local Commands = {}
 
 -- Find a command.
 function Commands.findCommand(call)
+	-- Commands are not case-sensitive.
+	call = string.lower(call)
 
+	-- Handle reverse commands.
+	local procedure = "invoke"
+	if string.sub(call, 1, #REVOKE_PREFIX) == REVOKE_PREFIX then
+		procedure = "revoke"
+	end
+
+	for _, command in ipairs(Commands.cache) do
+		-- Check if command's name matches.
+		if string.lower(command.name) == call then
+			return command, procedure
+		end
+
+		-- Check command's aliases.
+		for _, aliase in ipairs(command.aliases or {}) do
+			if string.lower(aliase) == call then
+				return command, procedure
+			end
+		end
+
+		-- Check for special opposites.
+		for _, opposite in ipairs(command.opposites or {}) do
+			if string.lower(opposite) == call then
+				procedure = "revoke"
+				return command, procedure
+			end
+		end
+	end
 end
 
 return Commands
